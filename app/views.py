7b2a1dbe9d254fa07ca27ceaecf2361f0d196d37
taras_shapik <shapiktaras@gmail.com>
2023-12-23 16:53:57 +0200

    base apiview create

--- app/views.py
+++ app/views.py
@@ -1,9 +1,26 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from rest_framework.response import Response
+
 from .models import *
 from .serializers import InfoSerializer
+from rest_framework.views import APIView
 
 
-class InfoAPIView(generics.ListAPIView):
-    queryset = Info.objects.all()
-    serializer_class = InfoSerializer
+# class InfoAPIView(generics.ListAPIView):
+#     queryset = Info.objects.all()
+#     serializer_class = InfoSerializer
 
+
+class InfoAPIView(APIView):
+    def get(self, request):
+        lst = Info.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Info.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id']
+        )
+        return Response({'posts': model_to_dict(post_new)})
