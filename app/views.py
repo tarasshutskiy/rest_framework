5b07a21003f8a94bf9f7640751622c7c9a793152
taras_shapik <shapiktaras@gmail.com>
2023-12-25 08:21:04 +0200

    paginator

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
@@ -29,10 +30,19 @@ from rest_framework.views import APIView
 
 
 """BASE GENERICS APIVIEWS"""
+
+
+class InfoAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class InfoAPIList(generics.ListCreateAPIView):
     queryset = Info.objects.all()
     serializer_class = InfoSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = InfoAPIListPagination
 
 
 class InfoAPIUpdate(generics.RetrieveUpdateAPIView):
